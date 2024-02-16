0b7e1eec98b250ed4d48ed728f2ee831e90fc99a
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-02-16 11:53:38 +0700

    clean: clean index ts

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,19 @@
-import { Request, Response } from "express";
-import data from "./db/db.json";
+import { NextFunction, Request, Response } from "express";
 import bodyParser from "body-parser";
 import express from "express";
+import BookRouter from "./routes/bookRoute";
 
 // server.js
 const app = express();
 
-const books: { id: string; name: string }[] = data;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World");
 });
 
-app.get("/books", (req: Request, res: Response) => {
-  res.json(books);
-});
-
-app.get("/books/:id", (req: Request, res: Response) => {
-  res.json(books.find((book) => book.id === req.params.id));
-});
+app.use("/books", BookRouter);
 
 app.listen(3000, () => {
   console.log("http://localhost:3000");
