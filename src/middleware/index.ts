f2676f45798f7ffe281632aa033e629f1e6c3e18
Traiphakh stihhikaew <sirprak1245@gmail.com>
2024-02-16 10:58:12 +0700

    imp: add log middleware

---
+++ src/middleware/index.ts
@@ -0,0 +1,10 @@
+type ExpressMiddleware = (
+  req: Request,
+  res: Response,
+  next: () => void
+) => void;
+
+const logger: ExpressMiddleware = (req, res, next) => {
+  console.log(`Request URL: ${req.url}`);
+  next();
+};
